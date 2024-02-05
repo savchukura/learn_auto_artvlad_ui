060a90cb5ef45026afffbb6b5a3d3604a0b47b33
savcukura <savcukura866@gmail.com>
2024-02-05 19:05:56 +0200

    change conftest.py

--- conftest.py
+++ conftest.py
@@ -11,7 +11,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 def driver():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(options=chrome_options, service=ChromeService(ChromeDriverManager().install()))
+    driver = webdriver.Chrome('/usr/bin/chromedriver')
+    #driver = webdriver.Chrome(options=chrome_options, service=ChromeService(ChromeDriverManager().install()))
     #driver.maximize_window()
 
     yield driver
